1305123b377bc302090b8a9e33c6453cd7e21ee0
scfmod <espen@bimbear.com>
2022-01-09 12:47:01 +0100

    Custom density height map support for DIRT

--- classes/TerraFarmFillTypes.lua
+++ classes/TerraFarmFillTypes.lua
@@ -47,5 +47,32 @@ function TerraFarmFillTypes:add(name)
         table.insert(self.TYPES_LIST, title)
         self.NAME_TO_INDEX[name] = #self.TYPES_LIST
         self.TITLE_TO_NAME[title] = name
+
+        if not g_densityMapHeightManager.fillTypeNameToHeightType[name] then
+            if self:addDensityMapHeight(name) then
+                Logging.info('TerraFarm: Successfully added custom density map height - ' .. tostring(name))
+            end
+        end
     end
+end
+
+function TerraFarmFillTypes:addDensityMapHeight(fillTypeName)
+    local heightType = g_densityMapHeightManager.fillTypeNameToHeightType[fillTypeName] or {}
+    local maxAngle = 41
+    local maxSurfaceAngle = heightType.maxSurfaceAngle or math.rad(26)
+
+    if maxAngle ~= nil then
+        maxSurfaceAngle = math.rad(maxAngle)
+    end
+
+    local fillToGroundScale = 1.0
+    local allowsSmoothing = true
+    local collisionScale = 1.0
+    local collisionBaseOffset = 0.08
+    local minCollisionOffset = 0.0
+    local maxCollisionOffset = 0.08
+
+    local isBaseType = false
+
+    return g_densityMapHeightManager:addDensityMapHeightType(fillTypeName, maxSurfaceAngle, collisionScale, collisionBaseOffset, minCollisionOffset, maxCollisionOffset, fillToGroundScale, allowsSmoothing, isBaseType)
 end
